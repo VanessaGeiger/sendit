67ed2bacfcb3cbd9bd97e55a0c0479738fb6304c
Daniel Stoeck <d.stoeck@otterbach.de>
2014-09-26 12:49:29 +0200

    code import

--- SendIT/src/admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	
-	if(isset($_SESSION["admin"]) && $_SESSION["admin"] == true) {
-		$_SESSION = array();
-		session_destroy();
-		header ("Location: index.php"); 
-	}
-?>
--- SendIT/src/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	
-	if(isset($_SESSION["angemeldet"]) && $_SESSION["angemeldet"] == true) {
-		$_SESSION = array();
-		session_destroy();
-		header ("Location: ../index.php"); 
-	}
-?>
