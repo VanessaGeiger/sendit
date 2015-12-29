fd0cc8127b10e37abfceba533aace1143a98498e
Vanessa Geiger <v.geiger@otterbach.de>
2015-12-29 11:54:54 +0100

    Fehler im Refactoring beseitig :)

--- v3/app/Http/Controllers/HistoryController.php
+++ v3/app/Http/Controllers/HistoryController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class VerlaufController extends Controller
+class HistoryController extends Controller
 {
     public function index() {
         return view('history');
