a6b3cb9ea179472fb28dba4f7b55f3e29525bf75
Vanessa Geiger <v.geiger@otterbach.de>
2016-01-04 16:17:05 +0100

    Verlauf Blade Gestaltung

--- v3/resources/views/history.blade.php
+++ v3/resources/views/history.blade.php
@@ -11,6 +11,42 @@
               <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
               <li class="active">Here</li>
           </ol>--}}
+
+        <ul class="timeline">
+
+            <!-- timeline time label -->
+            <li class="time-label">
+        <span class="bg-red">
+            04 Jan. 2015
+        </span>
+            </li>
+            <!-- /.timeline-label -->
+
+            <!-- timeline item -->
+            <li>
+                <!-- timeline icon -->
+                <i class="fa fa-envelope bg-green"></i>
+                <div class="timeline-item">
+                    <span class="time"><i class="fa fa-clock-o"></i> 16:11</span>
+
+                    <h3 class="timeline-header"><a href="#">SendIt Team</a> </h3>
+
+                    <div class="timeline-body">
+                        ...
+                        Es wird :)
+                    </div>
+
+                    <div class="timeline-footer">
+                        <a class="btn btn-primary btn-xs">...</a>
+                    </div>
+                </div>
+            </li>
+            <!-- END timeline item -->
+
+
+
+        </ul>
+
     </section>
 
     <!-- Main content -->
